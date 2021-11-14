7bff138ff059b50061793ffc9c236219fdaaa076
RLX <85926055+RLX-OP@users.noreply.github.com>
2021-11-14 14:26:33 +0530

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -132,8 +132,8 @@ let footer = $(`
              <div class="form-header">
                 <h6 class="display">Get in Touch</h6>
               </div>
-                // <form>
-                  <form action="https://formcarry.com/s/nHfogoxv8-O" method="POST" accept-charset="UTF-8" >
+                //<form>
+                  <form action="https://formcarry.com/s/nHfogoxv8-O" method="POST" accept-charset="UTF-8">
                   <input type="text" name="field1" placeholder="Your Name" required/>
                   <input type="email" name="field2" placeholder="Email Address"  required/>
                   <textarea name="field3" placeholder="Type your Message" required></textarea>
