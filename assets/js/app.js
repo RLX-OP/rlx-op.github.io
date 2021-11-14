88cb279b7abf5213e33e06832056eecfd4a77a7a
RLX <85926055+RLX-OP@users.noreply.github.com>
2021-11-14 12:48:52 +0530

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -139,6 +139,7 @@ let footer = $(`
                   <input type="email" name="field2" placeholder="Email Address"  required/>
                   <textarea name="field3" placeholder="Type your Message" required></textarea>
                   <input type="submit" value="Send" />
+                  <input type="hidden" name="_gotcha">
                 </form>
              </div>
           </div>
