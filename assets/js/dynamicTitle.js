fa684eaf390a154321b87e8c5c73c02695376ed8
RLX <85926055+RLX-OP@users.noreply.github.com>
2021-11-21 16:53:53 +0530

    Update dynamicTitle.js

--- assets/js/dynamicTitle.js
+++ assets/js/dynamicTitle.js
@@ -16,17 +16,17 @@ window.onload = function () {
   document.addEventListener("visibilitychange", function (e) {
     if (!document.hidden) {
       document.title = pageTitle;
-      favicon.href = "/../../rlx.jpg";
+      favicon.href = "../../rlx.jpg";
     }
   });
 
   function toggle() {
     if (document.title === attentionMessage) {
         document.title = pageTitle;
-        favicon.href = "/../../rlx.jpg";
+        favicon.href = "../../rlx.jpg";
     } else {
         document.title = attentionMessage;
-        favicon.href = "/../../rlx.jpg";
+        favicon.href = "../../rlx.jpg";
     }
   }   
 };
