cdcd7e2c5038584414716c090b8e05d067572a7c
RLX <85926055+RLX-OP@users.noreply.github.com>
2021-12-01 21:29:20 +0530

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -220,7 +220,7 @@ let footer = $(`
 
 
 
-    <a class="social-button instagram" href="https://instagram.com/rlxop99" target="_blank" >
+    <a class="social-button instagram" href="https://instagram.com/ig_rlx" target="_blank" >
     <!-- svg code for instagram icon -->
       <svg class="instagram-icon-footer" x="0px" y="0px" viewBox="0 0 202.5 202.5"
         style="enable-background:new 0 0 202.5 202.5;">
